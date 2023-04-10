e86de45b93f0663676729c17d9c3c32190a128c9
akram <akramhany65@gmail.com>
2023-04-10 17:11:30 +0200

    Implementing methods of class Process except for NeedIO (it needs timeInfo struct) editing small things and adding a new paramter in Process.h

--- src/entity/Process.cpp
+++ src/entity/Process.cpp
@@ -1,1 +1,37 @@
 #include "Process.h"
+
+Process::Process(int id, unsigned int ioNum): PID(id), IONumOfReq(ioNum), descendant(nullptr), IOCounter(0)
+{
+}
+
+int Process::GetID() const
+{
+	return PID;
+}
+
+Process* Process::GetDescendant() const
+{
+	return descendant;
+}
+
+void Process::SetDescendant(Process* child)
+{
+	descendant = child;
+}
+
+unsigned int Process::GetIONumOfReq() const
+{
+	return IONumOfReq;
+}
+
+void Process::SetIONumOfReq(unsigned int ioNum)
+{
+	IONumOfReq = ioNum;
+}
+
+bool Process::NeedIO() const
+{
+	return false;
+}
+
+
--- src/entity/Process.h
+++ src/entity/Process.h
@@ -9,17 +9,19 @@ private:
 	const int PID;
 	Process* descendant;
 	unsigned int IONumOfReq;
+	unsigned int IOCounter;			//keep track of the current IO time and duration (as one process can have many IO)
 public:
 	Process(int id, unsigned int ioNum = 0);
 
 	int GetID() const;
 	Process* GetDescendant() const;
-	void SetDescendant(Process* Child);
+	void SetDescendant(Process* child);
 	unsigned int GetIONumOfReq() const;			//can be used to know if the process would request IO or not
 	void SetIONumOfReq(unsigned int ioNum);
 
 	//returns true if the process needs IO in this timestep
 	//You may need to change its parameters depending on your implementation (ex: you may want to send the time to it)
+	//You may also remove this method and just get the timeInfo of the process to check for IO time
 	bool NeedIO() const;						
 };
 
