55db3beaa4a2783b9d9a9c090e9bc9622e697d04
akram <akramhany65@gmail.com>
2023-04-10 16:02:05 +0200

    Write the main attributes and methods headers of class Process

--- src/entity/Process.h
+++ src/entity/Process.h
@@ -5,5 +5,21 @@
 ///=//////////////////////////////////////////////////////////=///
 class Process
 {
+private:
+	const int PID;
+	Process* descendant;
+	unsigned int IONumOfReq;
+public:
+	Process(int id, unsigned int ioNum = 0);
+
+	int GetID() const;
+	Process* GetDescendant() const;
+	void SetDescendant(Process* Child);
+	unsigned int GetIONumOfReq() const;			//can be used to know if the process would request IO or not
+	void SetIONumOfReq(unsigned int ioNum);
+
+	//returns true if the process needs IO in this timestep
+	//You may need to change its parameters depending on your implementation (ex: you may want to send the time to it)
+	bool NeedIO() const;						
 };
 
