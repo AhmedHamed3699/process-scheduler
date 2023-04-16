bc15d076116a55e64a13a7a9fe6a29e1a872a3bd
akram <akramhany65@gmail.com>
2023-04-17 00:36:42 +0200

    Write the functions empty implementation except for AddProcessToList function.

--- src/entity/ProcessorRR.cpp
+++ src/entity/ProcessorRR.cpp
@@ -1,1 +1,23 @@
 #include "ProcessorRR.h"
+
+void ProcessorRR::IOHandler()
+{
+}
+
+void ProcessorRR::WorkStealingHandler()
+{
+}
+
+void ProcessorRR::MigratonHandler()
+{
+}
+
+Process* ProcessorRR::ExecuteProcess()
+{
+    return nullptr;
+}
+
+void ProcessorRR::AddProcessToList(Process* process)
+{
+    readyList.enqueue(process);
+}
