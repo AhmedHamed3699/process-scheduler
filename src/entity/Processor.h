62d9c3535b252818e348ea6f06027c30a38a20dd
akram <akramhany65@gmail.com>
2023-04-16 14:55:17 +0200

    Add some comments to Processor.h and make the destructor virtual.

--- src/entity/Processor.h
+++ src/entity/Processor.h
@@ -10,16 +10,19 @@
 class Processor
 {
 protected:
-	Scheduler* scheduler;
-	ProcessorStatus status;
-	Process* currentProcess;
-	unsigned int totalBusyTime;
-	unsigned int expectedFinishTime;
+	Scheduler* scheduler;				//Pointer to Scheduler
+	ProcessorStatus status;				//current state of the Processor
+	Process* currentProcess;			//the process being executed right now by the processor
+	unsigned int totalBusyTime;			//the total time the processor was busy (executing a process)
+	unsigned int expectedFinishTime;	//expected finish time of the current process
 
-	virtual void IOHandler() = 0;
-	virtual void WorkStealingHandler() = 0;
+	virtual void IOHandler() = 0;				//it manages how the I/O for a process would happen
+	virtual void WorkStealingHandler() = 0;		//it manages how the work stealing between processors would happen
 public:
 	Processor(Scheduler* outScheduler);
+	virtual ~Processor();
+
+	//Setters and Getters for the data members
 	virtual ProcessorStatus GetStatus() const;
 	virtual void SetStatus(ProcessorStatus outStatus);
 	virtual Process* GetCurrentProcess() const;
@@ -29,9 +32,8 @@ public:
 	virtual unsigned int GetExpectedFinishTime() const;
 	virtual void SetExpectedFinishTime(unsigned int time);
 
-	virtual Process* ExecuteProcess() = 0;
-	virtual void AddProcessToList(Process process) = 0;
+	virtual Process* ExecuteProcess() = 0;					//The function responsible for executing a process
+	virtual void AddProcessToList(Process process) = 0;		//Adds a Process to the list of a Processor 
 
-	~Processor();
 };
 
