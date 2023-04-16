47f682a0d4714d4a63b712353d236cc8b2df125c
akram <akramhany65@gmail.com>
2023-04-16 14:32:39 +0200

    Add Functions Headers and ProcessorStatus to class Processor

--- src/entity/Processor.h
+++ src/entity/Processor.h
@@ -5,15 +5,31 @@
 ///=////////////////////////////////////////////=///
 #include "../control/Scheduler.h"
 #include "Process.h"
+#include "../data.h"
 
 class Processor
 {
 protected:
 	Scheduler* scheduler;
+	ProcessorStatus status;
 	Process* currentProcess;
 	unsigned int totalBusyTime;
 	unsigned int expectedFinishTime;
-public:
 
+	virtual void IOHandler() = 0;
+	virtual void WorkStealingHandler() = 0;
+public:
+	Processor(Scheduler* outScheduler);
+	virtual ProcessorStatus GetStatus() const;
+	virtual void SetStatus(ProcessorStatus outStatus);
+	virtual Process* GetCurrentProcess() const;
+	virtual void SetCurrentProcess(Process* outProcess);
+	virtual unsigned int GetTotalBusyTime() const;
+	virtual void SetTotalBusyTime(unsigned int time);
+	virtual unsigned int GetExpectedFinishTime() const;
+	virtual void SetExpectedFinishTime(unsigned int time);
+
+	virtual Process* ExecuteProcess() = 0;
+	virtual void AddProcessToList(Process process) = 0;
 };
 
