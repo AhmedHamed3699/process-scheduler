34747737795e9e362a5303ac6b127abfff1ba34d
akram <akramhany65@gmail.com>
2023-04-16 14:12:43 +0200

    Add the data members of class Processor

--- src/entity/Processor.h
+++ src/entity/Processor.h
@@ -3,7 +3,17 @@
 ///  Processor Class: runs and stores processes  ///
 /// Created By scheduler maintained by scheduler ///
 ///=////////////////////////////////////////////=///
+#include "../control/Scheduler.h"
+#include "Process.h"
+
 class Processor
 {
+protected:
+	Scheduler* scheduler;
+	Process* currentProcess;
+	unsigned int totalBusyTime;
+	unsigned int expectedFinishTime;
+public:
+
 };
 
