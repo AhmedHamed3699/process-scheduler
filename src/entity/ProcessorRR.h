e770af79e4fcb90db706b5f8b61f84bb7ea4a460
akram <akramhany65@gmail.com>
2023-04-17 00:35:00 +0200

    Create the .h and .cpp files of ProcessorRR. Add the data members and functions headers to it.

---
+++ src/entity/ProcessorRR.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Processor.h"
+#include "Process.h"
+#include "../ds/Queue.h"
+
+class ProcessorRR : public Processor
+{
+private:
+	Queue<Process*> readyList;
+
+	void IOHandler();				//it manages how the I/O for a process would happen
+	void WorkStealingHandler();		//it manages how the work stealing between processors would happen
+	void MigratonHandler();			//it manages how the migration between processors would happen
+
+public:
+	Process* ExecuteProcess();					//The function responsible for executing a process
+	void AddProcessToList(Process* process);		//Adds a Process to the list of a Processor 
+};
+
