f3534b684ab7676b7460a593acd50c175b1b4c6b
akram <akramhany65@gmail.com>
2023-04-17 00:24:31 +0200

    Write the empty functions implementation in ProcessorFCFS.cpp.

--- src/entity/ProcessorFCFS.cpp
+++ src/entity/ProcessorFCFS.cpp
@@ -1,1 +1,30 @@
 #include "ProcessorFCFS.h"
+
+void ProcessorFCFS::IOHandler()
+{
+}
+
+void ProcessorFCFS::WorkStealingHandler()
+{
+}
+
+void ProcessorFCFS::MigratonHandler()
+{
+}
+
+void ProcessorFCFS::KillProcessHandler()
+{
+}
+
+void ProcessorFCFS::ForkHandler()
+{
+}
+
+Process* ProcessorFCFS::ExecuteProcess()
+{
+	return nullptr;
+}
+
+void ProcessorFCFS::AddProcessToList(Process* process)
+{
+}
