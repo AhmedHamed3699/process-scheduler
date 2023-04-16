4a7ef05aad239f461bc2cb160768298c02a11f66
akram <akramhany65@gmail.com>
2023-04-16 16:50:27 +0200

    Write the data members and functions headers of ProcessorSJF.

--- src/entity/ProcessorSJF.h
+++ src/entity/ProcessorSJF.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "Processor.h"
 #include "../ds/PriorityQueue.h"
+#include "Process.h"
 
 class ProcessorSJF : public Processor
 {
+private:
+	PriorityQueue<Process*> readyList;
+	void IOHandler();				//it manages how the I/O for a process would happen
+	void WorkStealingHandler();		//it manages how the work stealing between processors would happen
 
+public:
+	Process* ExecuteProcess();					//The function responsible for executing a process
+	void AddProcessToList(Process process);		//Adds a Process to the list of a Processor 
 };
 
