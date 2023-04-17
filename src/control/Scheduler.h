c703aef41a17a1040257346699803ed3edb141bc
amir-kedis <amir.kedis@gmail.com>
2023-04-17 11:28:45 +0200

    🐛 create an empty constructor for simulator to build

--- src/control/Scheduler.h
+++ src/control/Scheduler.h
@@ -2,8 +2,21 @@
 ///=///////////////////////////////////////////////////=///
 /// Scheduler: Main Class and controller of the program ///
 ///=///////////////////////////////////////////////////=///
+
+#include "../data.h"
+
 class Scheduler
 {
 
+private:
+	SimulationParameters simulationParameters;
+
+
+public:
+	Scheduler()
+		:simulationParameters(0, 0, 0, 0, 0, 0, 0, 0, 0)
+	{
+
+	}
 };
 
