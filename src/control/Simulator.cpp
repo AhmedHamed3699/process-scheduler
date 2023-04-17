e72b14aee468543937e5d7b33da13eb0f5350cb3
amir-kedis <amir.kedis@gmail.com>
2023-04-17 19:29:10 +0200

    🔥 change testing function

--- src/control/Simulator.cpp
+++ src/control/Simulator.cpp
@@ -17,8 +17,31 @@ bool Simulator::CreateOutputFile()
 
 void Simulator::Run()
 {
+	/// TODO: This is just a test code. It will be removed later.
 	ui.PrintHeadline();
 	ui.PrintUIModeMenu();
+	ui.PrintTimeStamp();
+
+	scheduler.CreateProcessor(FCFS);
+	scheduler.CreateProcessor(SJF);
+	scheduler.CreateProcessor(RR);
+	scheduler.CreateProcessor(FCFS);
+	scheduler.CreateProcessor(SJF);
+
+	scheduler.CreateNewProcess(0);
+	scheduler.CreateNewProcess(2);
+	scheduler.CreateNewProcess(3);
+	scheduler.CreateNewProcess(4);
+	scheduler.CreateNewProcess(5);
+	scheduler.CreateNewProcess(6);
+	scheduler.CreateNewProcess(7);
+	scheduler.CreateNewProcess(8);
+	scheduler.CreateNewProcess(9);
+	scheduler.CreateNewProcess(10);
+	scheduler.CreateNewProcess(11);
+
+	clk.Step();
+
 	ui.PrintTimeStamp();
 	Simulation();
 }
