06ed14f974a8f23437f05764222f375d6fd31152
amir-kedis <amir.kedis@gmail.com>
2023-04-17 20:18:56 +0200

    🔨 create terminate process

--- src/control/Scheduler.cpp
+++ src/control/Scheduler.cpp
@@ -132,6 +132,16 @@ void Scheduler::ScheduleNextRR(Process* process)
 
 void Scheduler::TerminateProcess(Process* process)
 {
+	/// TODO: remove later
+	if (NEWList.isEmpty())
+	{
+		return;
+	}
+	process = NEWList.peekFront();
+	NEWList.dequeue();
+
+
+	TRMList.enqueue(process);
 }
 
 void Scheduler::BlockProcess(Process* process)
