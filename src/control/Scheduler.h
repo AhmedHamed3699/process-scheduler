a2affcba4516aafb0cf863a883ba1f702420eaef
amir-kedis <amir.kedis@gmail.com>
2023-04-17 12:44:34 +0200

    🔪 testing simulation Parameters struct Creation and deletion and assigning

--- src/control/Scheduler.h
+++ src/control/Scheduler.h
@@ -4,8 +4,8 @@
 ///=///////////////////////////////////////////////////=///
 
 #include "../data.h"
-#include "ds/List.h"
-#include "ds/Queue.h"
+#include "../ds/List.h"
+#include "../ds/Queue.h"
 #include "../entity/Process.h"
 #include "../entity/Processor.h"
 
@@ -20,7 +20,7 @@ private:
 	SimulationParameters simulationParameters;
 
 	/// ////////////////////////////////// ///
-	///           Scheduler Lists           ///
+	///           Scheduler Lists          ///
 	/// ////////////////////////////////// ///
 private:
 	List<Processor*> processors;
@@ -31,9 +31,17 @@ private:
 
 public:
 	Scheduler()
-		:simulationParameters(0, 0, 0, 0, 0, 0, 0, 0, 0)
+		:simulationParameters(1, 22, 33, 44, 55, 66, 77, 88, 424)
 	{
-
+		// TODO: reomve later
+		cout << "fork prob " << simulationParameters.FORK_PROBABILITY << endl;
+		cout << "mwt " << simulationParameters.MAX_WAITING_TIME << endl;
+		cout << "n fcfs " << simulationParameters.N_FCFS << endl;
+		cout << "n rr " << simulationParameters.N_RR << endl;
+		cout << "n sjf " << simulationParameters.N_SJF << endl;
+		cout << "n process " << simulationParameters.N_PROCESS << endl;
+		cout << "rtf " << simulationParameters.RTF << endl;
+		cout << "stl " << simulationParameters.STL << endl;
 	}
 };
 
