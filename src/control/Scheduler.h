8f5a247bb7614e101df5b929f00167e2ecd2e43e
amir-kedis <amir.kedis@gmail.com>
2023-04-17 11:36:53 +0200

    ✨ create Scheduler Lists and data members

--- src/control/Scheduler.h
+++ src/control/Scheduler.h
@@ -4,13 +4,30 @@
 ///=///////////////////////////////////////////////////=///
 
 #include "../data.h"
+#include "ds/List.h"
+#include "ds/Queue.h"
+#include "../entity/Process.h"
+#include "../entity/Processor.h"
+
 
 class Scheduler
 {
 
+	/// ////////////////////////////////// ///
+	///  Scheduler Data Members and flags  ///
+	/// ////////////////////////////////// ///
 private:
 	SimulationParameters simulationParameters;
 
+	/// ////////////////////////////////// ///
+	///           Scheduler Lists           ///
+	/// ////////////////////////////////// ///
+private:
+	List<Processor*> processors;
+	Queue<Process*> NEWList;
+	Queue<Process*> TRMList;
+	Queue<Process*> BLKList;
+	Process* IOProcess;
 
 public:
 	Scheduler()
