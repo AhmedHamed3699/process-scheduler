c33678f5afff025ce14a1cc8bc48ff67d016cc45
amir-kedis <amir.kedis@gmail.com>
2023-04-17 10:44:13 +0200

    💅 create set and reset colors functions

--- src/control/UI.cpp
+++ src/control/UI.cpp
@@ -1,1 +1,25 @@
 #include "UI.h"
+
+
+/// Output Functions
+void UI::WriteWarning(std::string msg)
+{
+}
+
+void UI::WriteError(std::string msg)
+{
+}
+
+
+/// Color Control Functions
+void UI::ResetColor()
+{
+	HANDLE consoleOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(consoleOutputHandle, DEFAULT_WHITE);
+}
+
+void UI::SetColor(Color color)
+{
+	HANDLE consoleOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(consoleOutputHandle, color);
+}
