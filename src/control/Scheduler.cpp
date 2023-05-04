3b6704e50a6d426a6a0193f9bda1514f51dfacd9
amir-kedis <amir.kedis@gmail.com>
2023-05-04 09:08:38 +0300

    🔨 make the execute processor run even if idle
    # any check of status can be run internally

--- src/control/Scheduler.cpp
+++ src/control/Scheduler.cpp
@@ -278,12 +278,8 @@ void Scheduler::RunProcesses()
 	for (int i = 0; i < processors.GetLength(); i++)
 	{
 		Processor* processor = processors.GetEntry(i + 1);
-		if (processor->GetStatus() == IDLE)
-		{
 			processor->ExecuteProcess(clk->GetTime());
 		}
-
-	}
 }
 
 void Scheduler::MoveToRDY(Process* process)
