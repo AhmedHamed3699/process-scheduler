b16101c41c47733fc063f0af6158c7dd06ae6e60
amir-kedis <amir.kedis@gmail.com>
2023-05-04 08:41:50 +0300

    ✨ RR scheduling algorithm draft

--- src/entity/ProcessorRR.cpp
+++ src/entity/ProcessorRR.cpp
@@ -1,5 +1,6 @@
 #include "ProcessorRR.h"
 #include <string>
+#include "../control/scheduler.h"
 
 void ProcessorRR::IOHandler()
 {
@@ -14,29 +15,74 @@ void ProcessorRR::MigratonHandler()
 }
 
 ProcessorRR::ProcessorRR(Scheduler* outScheduler)
-	:Processor(outScheduler, RR)
+	:Processor(outScheduler, RR), RR_TIME_SLICE(this->scheduler->GetSimulationParameters().RR_TIME_SLICE)
 {
+	quantumCounter = this->scheduler->GetSimulationParameters().RR_TIME_SLICE;
 }
 
 bool ProcessorRR::ExecuteProcess(int CurrentTime)
 {
-	//TODO: remove this later
-	if (readyList.isEmpty())
-		return false;
+	////TODO: remove this later
+	//if (readyList.isEmpty())
+	//	return false;
 
-	Process* process = readyList.peekFront();
-	if (process->GetTimeInfo().AT == CurrentTime)
-		return false;
+	//Process* process = readyList.peekFront();
+	//if (process->GetTimeInfo().AT == CurrentTime)
+	//	return false;
 
-	readyList.dequeue();
-	currentProcess = process;
-	process->SetStatus(RUN);
-	SetStatus(BUSY);
+	//readyList.dequeue();
+	//currentProcess = process;
+	//process->SetStatus(RUN);
+	//SetStatus(BUSY);
 
-	TimeInfo timeInfo = process->GetTimeInfo();
-	timeInfo.RT = CurrentTime - timeInfo.AT;
+	//TimeInfo timeInfo = process->GetTimeInfo();
+	//timeInfo.RT = CurrentTime - timeInfo.AT;
 
-	process->SetTimeInfo(timeInfo);
+	//process->SetTimeInfo(timeInfo);
+
+	//return true;
+	// Check if there is a running process 
+	if (this->currentProcess == nullptr)
+	{
+		// schedule next process, unless there is no process in the ready list
+		if (readyList.isEmpty())
+			return false;
+
+		Process* process = readyList.peekFront();
+		readyList.dequeue();
+		currentProcess = process;
+		process->SetStatus(RUN);
+		SetStatus(BUSY);
+		return true;
+	}
+
+	// Check if the process is finished
+	if (currentProcess->GetTimeInfo().RCT == 0)
+	{
+		// Set the process as finished
+		currentProcess->SetStatus(TRM);
+		scheduler->TerminateProcess(currentProcess);
+		currentProcess = nullptr;
+		SetStatus(IDLE);
+		return true;
+	}
+
+	// if quantum is finished, add the process to the ready list
+	if (quantumCounter == 0) // add here the quantum counter
+	{
+		readyList.enqueue(currentProcess);
+		currentProcess->SetStatus(RDY);
+		currentProcess = nullptr;
+		SetStatus(IDLE);
+		quantumCounter = RR_TIME_SLICE;
+		return true;
+	}
+
+	// execute the process
+	currentProcess->DecrementRCT(); // decrement the RCT
+
+	// decrement the quantum counter
+	quantumCounter--;
 
 	return true;
 }
--- src/entity/ProcessorRR.h
+++ src/entity/ProcessorRR.h
@@ -3,10 +3,14 @@
 #include "Process.h"
 #include "../ds/Queue.h"
 
+
+
 class ProcessorRR : public Processor
 {
 private:
-	Queue<Process*> readyList;
+	Queue<Process*> readyList;			//the list of processes that are ready to be executed
+	const unsigned int RR_TIME_SLICE;	//the quantum time of the processor
+	unsigned int quantumCounter;		//the counter that counts the quantum time
 
 	void IOHandler();				//it manages how the I/O for a process would happen
 	void WorkStealingHandler();		//it manages how the work stealing between processors would happen
